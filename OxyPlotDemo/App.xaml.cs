226f4eaa47bd4224b47b23caf2215f6eca05629a
Charlie Crawford <crawfordnet@icloud.com>
2018-02-13 14:06:19 -0500

    * .gitignore: oxyplot lineseries plots
    * Main.cs:
    * Info.plist:
    * OxyPlotDemo.sln:
    * AppDelegate.cs:
    * ios.plot.png:
    * packages.config:
    * App.xaml:
    * MainActivity.cs:
    * packages.config:
    * Entitlements.plist:
    * App.xaml.cs:
    * android.plot.png:
    * OxyPlotDemo.iOS.csproj:
    * LaunchScreen.storyboard:
    * packages.config:
    * AboutAssets.txt:
    * OxyPlotDemo.csproj:
    * OxyPlotDemo.Droid.csproj:
    * OxyPlotDemoPage.xaml:
    * AssemblyInfo.cs:
    * Contents.json:
    * icon.png:
    * styles.xml:
    * Tabbar.axml:
    * AboutResources.txt:
    * Toolbar.axml:
    * OxyPlotDemoPage.xaml.cs:
    * Resource.designer.cs:
    * AndroidManifest.xml:
    * icon.png:
    * AssemblyInfo.cs:
    * icon.png:
    * icon.png:
    * Contents.json:

---
+++ OxyPlotDemo/App.xaml.cs
@@ -0,0 +1,39 @@
+﻿using OxyPlot;
+using OxyPlot.Xamarin.Forms;
+using Xamarin.Forms;
+
+namespace OxyPlotDemo
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            this.MainPage = new OxyPlotDemoPage
+            {
+                //Content = new PlotView
+                //{
+                //    Model = new PlotModel { Title = "Hello, Forms!" },
+                //    VerticalOptions = LayoutOptions.Fill,
+                //    HorizontalOptions = LayoutOptions.Fill,
+                //},
+            };
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ OxyPlotDemo/OxyPlotDemoPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using OxyPlot.Xamarin.Forms;
+using Xamarin.Forms;
+using System;
+using System.Collections.Generic;
+using OxyPlot;
+using OxyPlot.Series;
+using OxyPlot.Axes;
+
+namespace OxyPlotDemo
+{
+    public partial class OxyPlotDemoPage : ContentPage
+    {
+        public OxyPlotDemoPage()
+        {
+            InitializeComponent();
+            PlotView pv = new PlotView();
+            pv.VerticalOptions = LayoutOptions.Fill;
+            pv.HorizontalOptions = LayoutOptions.Fill;
+
+            List<DataPoint> points = new List<DataPoint>();
+
+            // LineSeries Plots:
+            // Define the data for the plot. 
+            // e.g. var y = Math.Pow(x, 2),
+            //      var y = Math.Cos(x),
+            //      var y = Math.Sin(x), etc.
+            // 
+            for (double x = -10; x <= 10; x = x + .1)
+            {
+                var y = Math.Sin(x);
+                points.Add(new DataPoint(x,y));
+            }
+
+            var model = new PlotModel { Title = "LineSeries" };
+            var lineSeries = new LineSeries { MarkerType = MarkerType.Cross };
+            lineSeries.Points.AddRange(points);
+            model.Series.Add(lineSeries);
+
+            // ScatterSeries plots:
+            //var model = new PlotModel { Title = "ScatterSeries" };
+            //var scatterSeries = new ScatterSeries { MarkerType = MarkerType.Circle };
+            //var r = new Random(314);
+            //for (int i = 0; i < 100; i++)
+            //{
+            //    var x = r.NextDouble();
+            //    var y = r.NextDouble();
+            //    var size = r.Next(5, 15);
+            //    var colorValue = r.Next(100, 1000);
+            //    scatterSeries.Points.Add(new ScatterPoint(x, y, size, colorValue));
+            //}
+            //model.Series.Add(scatterSeries);
+            //model.Axes.Add(new LinearColorAxis { Position = AxisPosition.Right, Palette = OxyPalettes.Jet(200) });
+
+            pv.Model = model;
+
+            this.Content = pv;
+
+        }
+    }
+}
